37149c3c420c14451551d144db67f25c7a3c5a91
Maryna Bobryk <maryna@MacBook-Pro-von-Maryna.local>
2025-07-13 20:42:56 +0200

    integrate Swagger UI and add OpenAPI spec for auth & contacts

--- src/validation/resetPasswordSchema.js
+++ src/validation/resetPasswordSchema.js
@@ -4,6 +4,6 @@ export const resetPasswordSchema = Joi.object({
   token: Joi.string().required(),
   password: Joi.string().min(8).required().messages({
     'any.required': 'Password is required',
-    'string.min': 'Name must be at least {#limit} characters long',
+    'string.min': 'Password must be at least {#limit} characters long',
   }),
 });
